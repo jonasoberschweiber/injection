2164deffa9f106da5b3367e8dda633ae69ecd8ad
mephory <mephory@mephory.com>
2011-09-14 20:31:02 +0200

    floating damage numbers

--- pointsbar.py
+++ pointsbar.py
@@ -12,14 +12,21 @@ class PointsBar:
             self.bar = pygame.image.load("gfx/hpbar_blue.png")
         else:
             self.bar = pygame.image.load("gfx/hpbar_red.png")
+        self.num_font = pygame.font.Font(pygame.font.get_default_font(), 16)
+        self.num_color = pygame.Color(255, 0, 0)
 
         self.animation_health = 100
         self.animation = False
+        self.num_animation = 0
+        self.num = 0
 
-    def damage_taken(self, new_health):
+    def damage_taken(self, new_health, dmg):
         self.animation_health = self.health
         self.health = new_health
         self.animation = True
+        self.num_animation = 1
+        self.num = -1*dmg
+        self.num_rect = self.fighter.real_rect().move(34, 80)
 
     def render(self, surface):
         bar_rect = self.rect.move(2, 2)
@@ -32,3 +39,10 @@ class PointsBar:
             self.animation_health -= 2
             if self.animation_health in [self.health, self.health - 1]:
                 self.animation = False
+
+        if self.num_animation > 0:
+            surface.blit(self.num_font.render(str(self.num), 1, self.num_color), self.num_rect)
+            self.num_rect = self.num_rect.move(0, -5)
+            self.num_animation += 1
+            if self.num_animation == 20:
+                self.num_animation = 0
